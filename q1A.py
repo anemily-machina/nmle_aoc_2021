01f47c6ceaeebadbfb5e6756b59dbb64830d521e
vsippola <vincent.sippola@gmail.com>
2021-12-01 13:42:57 -0500

    Create q1A.py

---
+++ q1A.py
@@ -0,0 +1,12 @@
+import io
+
+fname = "p1A.txt"
+with io.open(fname, 'r', encoding='utf-8') as input_stream: 
+	count = 0
+	prev = int(input_stream.readline())
+	for line in input_stream:
+		curr = int(line)
+		count += 1 if curr > prev else 0
+		prev = curr
+
+	print(count)
